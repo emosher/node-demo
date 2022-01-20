8558bdaebce67a9e3abe00682c601b9bac4ba0ff
Eric Mosher <emoshmosh@gmail.com>
2022-01-20 13:16:03 -0500

    Fix the build

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello VMware!')
 })
 
-app.listen(port, () => {,
+app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
